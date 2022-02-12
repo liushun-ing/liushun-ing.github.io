ae80ac8fb8e86c219ddb94b756897dc1130aff8f
liushun <1743760069@qq.com>
2022-02-12 21:15:52 +0800

    2022-02-12

---
+++ index.js
@@ -0,0 +1,15 @@
+import java_data from './data/java.js';
+
+var java_data_show = java_data;
+
+// 上一页
+var prev_page = document.getElementById('prev-page');       
+prev_page.onclick = function() {
+    alert('prev');
+}
+
+// 下一页
+var prev_page = document.getElementById('next-page');       
+prev_page.onclick = function() {
+    alert('next');
+}
--- java/Mybatis/Mybatis.md
+++ java/Mybatis/Mybatis.md
@@ -1,4 +1,4 @@
-Mybatis
+# Mybatis
 
 ## 简介
 
